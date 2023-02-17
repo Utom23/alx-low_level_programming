05d989da0e03e4ee1a94593f81ff8d831a701f53
Priscilla Aguonye <aguonyepriscilla866@gmail.com>
2023-02-17 03:45:37 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <stdio> 
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
- */
+ * main - Print a line with puts function
+ *
+ * Return: 0
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
